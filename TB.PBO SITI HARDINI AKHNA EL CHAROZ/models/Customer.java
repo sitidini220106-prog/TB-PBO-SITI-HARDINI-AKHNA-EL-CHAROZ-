090d53e2a4343562d81cca2bdc7296cfbc8b4381
sitidini220106-prog <sitidini220106@gmail.com>
2025-12-29 21:19:25 +0700

    Update Customer.java

--- TB.PBO SITI HARDINI AKHNA EL CHAROZ/models/Customer.java
+++ TB.PBO SITI HARDINI AKHNA EL CHAROZ/models/Customer.java
@@ -1,17 +1,24 @@
-package models;
+package models; // Package untuk model / entitas
 
+// Abstract class Customer sebagai superclass
 public abstract class Customer {
 
+    // Atribut nama pelanggan
     protected String name;
 
+    // Constructor untuk mengisi nama pelanggan
     public Customer(String name) {
         this.name = name;
     }
 
+    // Method untuk mengambil nama pelanggan
     public String getName() {
         return name;
     }
 
+    // Abstract method untuk diskon (akan dioverride)
     public abstract double getDiscount();
+
+    // Abstract method untuk tipe pelanggan
     public abstract String getCustomerType();
 }
