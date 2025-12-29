a9c90a47065a8167fa2673362ed51ebb5ca0a1d7
sitidini220106-prog <sitidini220106@gmail.com>
2025-12-29 21:21:45 +0700

    Update KiloanService.java

--- TB.PBO SITI HARDINI AKHNA EL CHAROZ/services/KiloanService.java
+++ TB.PBO SITI HARDINI AKHNA EL CHAROZ/services/KiloanService.java
@@ -1,13 +1,18 @@
 package services;
+
+// Implementasi interface LaundryService
 public class KiloanService implements LaundryService {
 
+    // Harga per kilogram
     private final double pricePerKg = 7000;
 
+    // Perhitungan harga laundry
     @Override
     public double calculatePrice(double qty) {
         return qty * pricePerKg;
     }
 
+    // Nama layanan
     @Override
     public String getServiceName() {
         return "Laundry Kiloan";
