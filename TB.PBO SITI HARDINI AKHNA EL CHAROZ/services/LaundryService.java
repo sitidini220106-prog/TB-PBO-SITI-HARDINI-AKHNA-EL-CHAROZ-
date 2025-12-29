0a7da71ade78bdb721d33edd2e0e94e087bce644
sitidini220106-prog <sitidini220106@gmail.com>
2025-12-29 21:22:19 +0700

    Update LaundryService.java

--- TB.PBO SITI HARDINI AKHNA EL CHAROZ/services/LaundryService.java
+++ TB.PBO SITI HARDINI AKHNA EL CHAROZ/services/LaundryService.java
@@ -1,6 +1,12 @@
 package services;
 
+// Interface LaundryService
 public interface LaundryService {
+
+    // Method untuk menghitung harga
     double calculatePrice(double qty);
+
+    // Method untuk mengambil nama layanan
     String getServiceName();
 }
+
