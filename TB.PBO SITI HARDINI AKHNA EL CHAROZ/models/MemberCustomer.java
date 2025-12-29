7132d7db442028208ebdf77b3c809b21faee83dc
sitidini220106-prog <sitidini220106@gmail.com>
2025-12-29 21:18:12 +0700

    Refactor MemberCustomer class for clarity

--- TB.PBO SITI HARDINI AKHNA EL CHAROZ/models/MemberCustomer.java
+++ TB.PBO SITI HARDINI AKHNA EL CHAROZ/models/MemberCustomer.java
@@ -1,16 +1,20 @@
 package models;
 
+// Class MemberCustomer mewarisi Customer
 public class MemberCustomer extends Customer {
 
+    // Constructor
     public MemberCustomer(String name) {
         super(name);
     }
 
+    // Diskon member 10%
     @Override
     public double getDiscount() {
-        return 0.1; // 10% diskon
+        return 0.1;
     }
 
+    // Tipe pelanggan Member
     @Override
     public String getCustomerType() {
         return "Member";
