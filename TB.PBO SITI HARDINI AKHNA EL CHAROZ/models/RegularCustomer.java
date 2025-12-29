f282805df57ef59fbd5f4e5c4848d5195d8baa8a
sitidini220106-prog <sitidini220106@gmail.com>
2025-12-29 21:16:51 +0700

    Implement RegularCustomer class extending Customer

--- TB.PBO SITI HARDINI AKHNA EL CHAROZ/models/RegularCustomer.java
+++ TB.PBO SITI HARDINI AKHNA EL CHAROZ/models/RegularCustomer.java
@@ -1,18 +1,24 @@
 package models;
 
+// Class RegularCustomer mewarisi Customer
 public class RegularCustomer extends Customer {
 
+    // Constructor memanggil constructor superclass
     public RegularCustomer(String name) {
         super(name);
     }
 
+    // Diskon pelanggan regular = 0%
     @Override
     public double getDiscount() {
         return 0.0;
     }
 
+    // Tipe pelanggan Regular
     @Override
     public String getCustomerType() {
         return "Regular";
     }
 }
+
+
